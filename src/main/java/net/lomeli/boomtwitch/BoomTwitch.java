dcae2190ba7f883db0b0672ad5d5eadf67643e1e
Lomeli12 <anthonylomeli11@gmail.com>
2017-03-09 00:21:38 -0800

    Remove debug and start using events

--- src/main/java/net/lomeli/boomtwitch/BoomTwitch.java
+++ src/main/java/net/lomeli/boomtwitch/BoomTwitch.java
@@ -4,15 +4,23 @@ import com.google.common.base.Strings;
 
 import net.lomeli.boombot.api.Addon;
 import net.lomeli.boombot.api.BoomAPI;
-import net.lomeli.boombot.api.data.EntityData;
+import net.lomeli.boombot.api.events.Event;
+import net.lomeli.boombot.api.events.bot.GuildEvent;
 import net.lomeli.boombot.api.events.bot.InitEvent;
+import net.lomeli.boombot.api.events.bot.PostInitEvent;
 import net.lomeli.boombot.api.events.bot.PreInitEvent;
+import net.lomeli.boombot.api.events.bot.data.DataEvent;
+import net.lomeli.boombot.api.nbt.NBTTagBase;
+import net.lomeli.boombot.api.nbt.NBTTagCompound;
 import net.lomeli.boombot.api.util.Logger;
 import net.lomeli.boomtwitch.lib.PostHelper;
 import net.lomeli.boomtwitch.twitch.ApiResponse;
 
 @Addon(addonID = "boombot_twitch_integration", name = "BoomBot Twitch Integration", version = "1.0.0")
 public class BoomTwitch {
+    public static final String DELAY_KEY = "CheckDelayMillis";
+    public static final String TWITCH_KEY = "TwitchClientID";
+    public static final String INTEGRATION_KEY = "TwitchIntegration";
     @Addon.Instance
     public static BoomTwitch INSTANCE;
 
@@ -24,29 +32,15 @@ public class BoomTwitch {
     @Addon.Event
     public void preInit(PreInitEvent event) {
         log = new Logger(event.getAddon().addonID());
-        boolean change = false;
-        EntityData data = BoomAPI.dataRegistry.getBoomBotData().getData("TwitchIntegration");
-        if (!data.hasKey("TwitchClientID")) {
-            keyFlag = false;
-            data.setString("TwitchClientID", "");
-            change = true;
-        } else {
-            twitchClientKey = data.getString("TwitchClientID");
-            keyFlag = !Strings.isNullOrEmpty(twitchClientKey);
-        }
-
-        if (!data.hasKey("TwitchClientID")) {
-            change = true;
-            data.setLong("CheckDelayMillis", 60000);
-            delay = 60000;
-        } else delay = data.getLong("CheckDelayMillis");
-
-        if (change) BoomAPI.dataRegistry.getBoomBotData().setData("TwitchIntegration", data);
+        BoomAPI.eventRegistry.registerEventHandler(INSTANCE);
     }
 
+    @Addon.Event
+    public void initEvent(InitEvent event) {
+    }
 
     @Addon.Event
-    public void init(InitEvent event) {
+    public void post(PostInitEvent event) {
         if (keyFlag) {
             ApiResponse response = PostHelper.getStreamInfo("PlayHearthstone", twitchClientKey);
             if (response == null) log.error("Could not get info on test channel");
@@ -54,5 +48,33 @@ public class BoomTwitch {
                 log.debug("%s is %s playing %s", "PlayHearthstone", response.getStream() == null ? "Offline" : "Online",
                         response.getStream() == null ? "nothing" : response.getStream().getGame());
         }
+    }
+
+    @Event.EventHandler
+    public void dataWriteEvent(DataEvent.DataWriteEvent event) {
+        NBTTagCompound addonData = new NBTTagCompound();
+        addonData.setLong(DELAY_KEY, delay);
+        addonData.setString(TWITCH_KEY, twitchClientKey);
+        event.getBotData().setTag(INTEGRATION_KEY, addonData);
+    }
+
+    @Event.EventHandler
+    public void dataReadEvent(DataEvent.DataReadEvent event) {
+        NBTTagCompound addonData = new NBTTagCompound();
+        if (event.getBotData().hasTag(INTEGRATION_KEY, NBTTagBase.TagType.TAG_COMPOUND)) addonData = event.getBotData().getTagCompound(INTEGRATION_KEY);
+        twitchClientKey = addonData.hasTag(TWITCH_KEY, NBTTagBase.TagType.TAG_STRING) ? addonData.getString(TWITCH_KEY) : "";
+        delay = addonData.hasTag(DELAY_KEY, NBTTagBase.TagType.TAG_LONG) ? addonData.getLong(DELAY_KEY) : 60000;
+        keyFlag = !Strings.isNullOrEmpty(twitchClientKey);
+        log.debug(twitchClientKey);
+    }
+
+    @Event.EventHandler
+    public void joinGuildEvent(GuildEvent.JoinedGuildEvent event) {
+
+    }
+
+    @Event.EventHandler
+    public void leaveGuildEvent(GuildEvent.LeaveGuildEvent event) {
+
     }
 }
--- src/main/java/net/lomeli/boomtwitch/twitch/UserChannelHandler.java
+++ src/main/java/net/lomeli/boomtwitch/twitch/UserChannelHandler.java
@@ -5,8 +5,12 @@ import com.google.common.collect.Maps;
 import java.util.List;
 import java.util.Map;
 
+import net.lomeli.boomtwitch.BoomTwitch;
+
 public class UserChannelHandler implements Runnable {
     private Map<String, List<String>> guildCache;
+    private long time;
+    private boolean isChecking;
 
     public UserChannelHandler() {
         guildCache = Maps.newHashMap();
@@ -14,6 +18,11 @@ public class UserChannelHandler implements Runnable {
 
     @Override
     public void run() {
+        while (true) {
+            long current = System.currentTimeMillis();
+            if (current < time + BoomTwitch.delay || isChecking) continue;
+            isChecking = true;
 
+        }
     }
 }
